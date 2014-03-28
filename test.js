ace5538a4447cf66a8160b8be2c97ac3feffc726
BenBlack <benjyblack@gmail.com>
2014-03-27 20:26:50 -0400

    before pairwise

--- test.js
+++ test.js
@@ -7,12 +7,15 @@ var bayes = require('./optimal-bayes'),
 	_ = require('lodash');
 
 
+var fileName;
 if (process.argv.length <= 2) {
 	winston.error('Filename argument required');
 	return;
 }
-
-var fileName = process.argv[2];
+else if (process.argv[1] === 'debug') {
+	fileName = process.argv[3];
+}
+else fileName = process.argv[2];
 
 var headerline = [];
 var dataSet = [];
@@ -40,11 +43,76 @@ csv()
 		var classifer = bayes();
 
 		// initialize data sets
-		var length = dataSet.length;
-		var trainingSet = dataSet.slice(0,length/2);
-		var testingSet = dataSet.slice(length/2, length);
+		var trainingSet = getTrainingSet(dataSet);
+		var testingSet = getTestingSet(dataSet);
 
+		var partitions = {};
+
+		// grab categories
+		var categories = getCategories(dataSet);
+
+		// create a partition for each category
+		categories.forEach(function(category) {
+			partitions[category] = { data: [], meanVector: {} };
+			partitions[category].data = getAllDataForCategory(trainingSet, category);
+			partitions[category].meanVector =  getMeanVector(partitions[category].data);
+		});
 	})
 	.on('error', function(error){
 		winston.error('Error parsing CSV file: ' + error.message );
 	});
+
+var getCategories = function(dataSet) {
+	var categories = [];
+
+	dataSet.forEach(function(data) {
+		if (categories.indexOf(data.category) === -1)
+			categories.push(data.category);
+	});
+
+	return categories;
+};
+
+var getTrainingSet = function(dataSet) {
+	return dataSet.slice(0,dataSet.length/2);
+};
+
+var getTestingSet = function(dataSet) {
+	return dataSet.slice(dataSet.length/2, dataSet.length);
+};
+
+var getAllDataForCategory = function(dataSet, category) {
+	var dataForCategory = [];
+
+	dataSet.forEach(function(data) {
+		if (data.category === category) {
+			// trim off the last field, the category name
+			// delete data.category;
+
+			dataForCategory.push(data);
+		}
+	});
+
+	if (dataForCategory.length === 0) debugger;
+
+	return dataForCategory;
+};
+
+var getMeanVector = function(data) {
+	var vector = {};
+
+	// sum all data point categories
+	data.forEach(function(d) {
+		for (var key in d) {
+			if (typeof(vector[key]) === 'undefined') vector[key] = 0;
+			vector[key] += Number(d[key]);
+		}
+	});
+
+	// average them out
+	for (var key in vector) {
+		vector[key] /= data.length;
+	}
+
+	return vector;
+};
