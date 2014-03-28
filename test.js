6772afcd653b68b0e09e5b635aab04662a583a07
BenBlack <benjyblack@gmail.com>
2014-03-28 11:09:16 -0400

    change to vector based instead of object based

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ var bayes = require('./optimal-bayes'),
 	winston = require('winston'),
 	csv = require('csv'),
 	fs = require('fs'),
-	_ = require('lodash');
+	_ = require('lodash'),
+	mathjs = require('mathjs'),
+	math = mathjs();
 
 
 var fileName, categoryToCompare;
@@ -54,17 +56,18 @@ csv()
 
 		// grab categories
 		var categories = getCategories(dataSet);
+		var k = getK(dataSet);
 
 		if (categories.indexOf(categoryToCompare) === -1) {
 			winston.error('Given class does not exist.');
 			return;
 		}
 
-
 		// Assembled categoryToCompare partition
 		partitions[categoryToCompare] = { data: [], meanVector: {} };
 		partitions[categoryToCompare].data = getAllDataForCategory(trainingSet, categoryToCompare);
 		partitions[categoryToCompare].meanVector =  getMeanVector(partitions[categoryToCompare].data);
+		partitions[categoryToCompare].covarianceMatrix = buildCovarianceMatrix(partitions[categoryToCompare]);
 
 		// Assemble other partition
 		partitions.other = { data: [], meanVector: {} };
@@ -74,6 +77,9 @@ csv()
 			}
 		});
 		partitions.other.meanVector =  getMeanVector(partitions.other.data);
+		partitions.other.covarianceMatrix = buildCovarianceMatrix(partitions.other);
+
+
 	})
 	.on('error', function(error){
 		winston.error('Error parsing CSV file: ' + error.message );
@@ -88,6 +94,12 @@ var getCategories = function(dataSet) {
 	});
 
 	return categories;
+};
+
+var getK = function(dataSet) {
+	var sample = dataSet[0];
+	// subtract 1 because one of the data points is the category itself
+	return Object.keys(sample).length - 1;
 };
 
 var getTrainingSet = function(dataSet) {
@@ -106,28 +118,51 @@ var getAllDataForCategory = function(dataSet, category) {
 			// trim off the last field, the category name
 			delete data.category;
 
-			dataForCategory.push(data);
+			var vector = [];
+
+			for (var item in data) {
+				vector.push(data[item]);
+			}
+
+			dataForCategory.push(vector);
 		}
 	});
 
 	return dataForCategory;
 };
 
-var getMeanVector = function(data) {
-	var vector = {};
+var getMeanVector = function(dataSet) {
+	var vector = [];
 
 	// sum all data point categories
-	data.forEach(function(d) {
-		for (var key in d) {
-			if (typeof(vector[key]) === 'undefined') vector[key] = 0;
-			vector[key] += Number(d[key]);
+	dataSet.forEach(function(dataPoint) {
+		for (var i = 0; i < dataPoint.length; i++) {
+			if (typeof(vector[i]) === 'undefined') vector[i] = 0;
+			vector[i] += Number(dataPoint[i]);
 		}
 	});
 
 	// average them out
-	for (var key in vector) {
-		vector[key] /= data.length;
+	for (var i = 0; i < vector.length; i++)
+	{
+		vector[i] /= dataSet.length;
 	}
 
 	return vector;
+};
+
+var buildCovarianceMatrix = function(partition) {
+	var covarianceMatrix = math.eval('zeros(k,k)', { k: k });
+
+
+	// partition.data.forEach(function(dataPoint) {
+
+	// });
+
+	return covarianceMatrix;
+};
+
+var subtractVectors = function(a, b) {
+	var result = {};
+
 };
