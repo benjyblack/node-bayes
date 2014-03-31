64f019c93e1342a2614b34ad52433addd1c5cc0b
BenBlack <benjyblack@gmail.com>
2014-03-31 10:41:33 -0400

    convert tabs to spaces and change size to 2

--- test/ten-fold.js
+++ test/ten-fold.js
@@ -2,119 +2,119 @@
 'use strict';
 
 var winston = require('winston'),
-	csv = require('csv'),
-	testFunctions = require('./test-functions'),
-	bayes = require('../lib/bayes');
+  csv = require('csv'),
+  testFunctions = require('./test-functions'),
+  bayes = require('../lib/bayes');
 
 var files = [
-	{
-		filename: 'iris',
-		categories: ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'],
-		results: {}
-	}, {
-		filename: 'heartDisease',
-		categories: ['0', '1', '2', '3', '4'],
-		results: {}
-	}, {
-		filename: 'wine',
-		categories: ['1', '2', '3'],
-		results: {}
-	}
+  {
+    filename: 'iris',
+    categories: ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'],
+    results: {}
+  }, {
+    filename: 'heartDisease',
+    categories: ['0', '1', '2', '3', '4'],
+    results: {}
+  }, {
+    filename: 'wine',
+    categories: ['1', '2', '3'],
+    results: {}
+  }
 ];
 
 var bayesTypes = ['optimal', 'naive', 'linear'];
 
 files.forEach(function (file) {
-	var headerline = [];
-	var dataSet = [];
+  var headerline = [];
+  var dataSet = [];
 
-	// read in file
-	csv()
-		.from.path(__dirname + '/data/' + file.filename + '.csv', { delimeter: ',', escape: '' })
-		.on('record', function(row,index) {
-			var i;
-			var data = {};
+  // read in file
+  csv()
+    .from.path(__dirname + '/data/' + file.filename + '.csv', { delimeter: ',', escape: '' })
+    .on('record', function(row,index) {
+      var i;
+      var data = {};
 
-			// First row will be the headerline
-			if (index === 0) {
-				for (i = 0; i < row.length; i++) headerline.push(row[i]);
-			}
-			else {
-				// Populate fields from csv
-				for (i = 0; i < row.length; i++) data[headerline[i]] = row[i];
+      // First row will be the headerline
+      if (index === 0) {
+        for (i = 0; i < row.length; i++) headerline.push(row[i]);
+      }
+      else {
+        // Populate fields from csv
+        for (i = 0; i < row.length; i++) data[headerline[i]] = row[i];
 
-				dataSet.push(data);
-			}
-		})
-		.on('end', function(){
-			winston.info('File: %s', file.filename);
+        dataSet.push(data);
+      }
+    })
+    .on('end', function(){
+      winston.info('File: %s', file.filename);
 
-			bayesTypes.forEach(function (bayesType) {
-				file.results[bayesType] = 0;
-				file.categories.forEach(function (primaryCategory) {
-					var total = 0;
-					var start = 0;
-					var middle, end;
-					var chunkLength = dataSet.length/10;
-					for (var i = 0; i < 10; i++)
-					{
-						// initialize classifier
-						var classifier = bayes();
+      bayesTypes.forEach(function (bayesType) {
+        file.results[bayesType] = 0;
+        file.categories.forEach(function (primaryCategory) {
+          var total = 0;
+          var start = 0;
+          var middle, end;
+          var chunkLength = dataSet.length/10;
+          for (var i = 0; i < 10; i++)
+          {
+            // initialize classifier
+            var classifier = bayes();
 
-						var organizedData, sets;
+            var organizedData, sets;
 
-						middle = start + chunkLength;
-						end = middle + chunkLength;
+            middle = start + chunkLength;
+            end = middle + chunkLength;
 
-						// repeat for as long as our categories are empty
-						do {
-							dataSet = testFunctions.shuffle(dataSet);
+            // repeat for as long as our categories are empty
+            do {
+              dataSet = testFunctions.shuffle(dataSet);
 
-							sets = testFunctions.partitionSets(dataSet, start, middle, end);
-							// get all categories
-							var categories = testFunctions.getCategories(dataSet);
+              sets = testFunctions.partitionSets(dataSet, start, middle, end);
+              // get all categories
+              var categories = testFunctions.getCategories(dataSet);
 
-							organizedData = testFunctions.organizeData(sets.trainingSet, primaryCategory, categories);
-						} while(organizedData.primaryCategoryData.length === 0 || organizedData.secondaryCategoryData.length === 0);
+              organizedData = testFunctions.organizeData(sets.trainingSet, primaryCategory, categories);
+            } while(organizedData.primaryCategoryData.length === 0 || organizedData.secondaryCategoryData.length === 0);
 
-						// teach the classifier about the data
-						classifier.teachPrimaryCategory(organizedData.primaryCategoryData);
-						classifier.teachSecondaryCategory(organizedData.secondaryCategoryData);
+            // teach the classifier about the data
+            classifier.teachPrimaryCategory(organizedData.primaryCategoryData);
+            classifier.teachSecondaryCategory(organizedData.secondaryCategoryData);
 
-						if (bayesType === 'naive')
-							classifier.naivify();
+            if (bayesType === 'naive')
+              classifier.naivify();
 
-						var numCorrect = 0;
-						var numWrong = 0;
+            var numCorrect = 0;
+            var numWrong = 0;
 
-						// Go through each data point in the testing set to see if our algorithm is correct
-						sets.testingSet.forEach(function(dataPoint) {
-							var c;
+            // Go through each data point in the testing set to see if our algorithm is correct
+            sets.testingSet.forEach(function(dataPoint) {
+              var c;
 
-							if (bayesType === 'linear') classifier.classifyLinear(dataPoint);
-							else c = classifier.classify(dataPoint);
+              if (bayesType === 'linear') classifier.classifyLinear(dataPoint);
+              else c = classifier.classify(dataPoint);
 
-							if (c > 0) {
-								if (dataPoint.category === primaryCategory) numCorrect++;
-								else numWrong++;
-							}
-							else {
-								if (dataPoint.category !== primaryCategory) numCorrect++;
-								else numWrong++;
-							}
-						});
+              if (c > 0) {
+                if (dataPoint.category === primaryCategory) numCorrect++;
+                else numWrong++;
+              }
+              else {
+                if (dataPoint.category !== primaryCategory) numCorrect++;
+                else numWrong++;
+              }
+            });
 
-						file.results[bayesType] += numCorrect/sets.testingSet.length*100;
+            file.results[bayesType] += numCorrect/sets.testingSet.length*100;
 
-						start += chunkLength;
-					}
-				});
-				winston.info('Algorithm: %s, Average percent: %d%', bayesType,file.results[bayesType]/file.categories.length/10);
-			});
+            start += chunkLength;
+          }
+        });
+        winston.info('Algorithm: %s, Average percent: %d%', bayesType,file.results[bayesType]/file.categories.length/10);
+      });
 
-			winston.info('\n');
-		})
-		.on('error', function(error){
-			winston.error('Error parsing CSV file: ' + error.message );
-		});
+      winston.info('\n');
+    })
+    .on('error', function(error){
+      winston.error('Error parsing CSV file: ' + error.message );
+    });
 });
--- test/test-functions.js
+++ test/test-functions.js
@@ -2,83 +2,83 @@
 'use strict';
 
 var organizeData = function(trainingSet, primaryCategory, categories) {
-	var primaryCategoryData, secondaryCategoryData;
+  var primaryCategoryData, secondaryCategoryData;
 
-	// get the data for the primary category and for the secondary category
-	primaryCategoryData = getAllDataForCategory(trainingSet, primaryCategory);
+  // get the data for the primary category and for the secondary category
+  primaryCategoryData = getAllDataForCategory(trainingSet, primaryCategory);
 
-	secondaryCategoryData = [];
-	categories.forEach(function(category) {
-		if (category !== primaryCategory) {
-			secondaryCategoryData = secondaryCategoryData.concat(getAllDataForCategory(trainingSet, category));
-		}
-	});
+  secondaryCategoryData = [];
+  categories.forEach(function(category) {
+    if (category !== primaryCategory) {
+      secondaryCategoryData = secondaryCategoryData.concat(getAllDataForCategory(trainingSet, category));
+    }
+  });
 
-	return {
-		primaryCategoryData: primaryCategoryData,
-		secondaryCategoryData: secondaryCategoryData
-	};
+  return {
+    primaryCategoryData: primaryCategoryData,
+    secondaryCategoryData: secondaryCategoryData
+  };
 };
 
 var partitionSets = function(dataSet, start, end) {
-	var testingSet = dataSet.slice(start, end);
-	var trainingSet = dataSet.diff(testingSet);
+  var testingSet = dataSet.slice(start, end);
+  var trainingSet = dataSet.diff(testingSet);
 
-	return {
-		trainingSet: trainingSet,
-		testingSet: testingSet
-	};
+  return {
+    trainingSet: trainingSet,
+    testingSet: testingSet
+  };
 };
 
 var shuffle = function (array) {
-	var currentIndex = array.length,
-		temporaryValue,
-		randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-	return array;
+  return array;
 };
 
 var getCategories = function(dataSet) {
-	var categories = [];
+  var categories = [];
 
-	dataSet.forEach(function(data) {
-		if (categories.indexOf(data.category) === -1)
-			categories.push(data.category);
-	});
+  dataSet.forEach(function(data) {
+    if (categories.indexOf(data.category) === -1)
+      categories.push(data.category);
+  });
 
-	return categories;
+  return categories;
 };
 
 var getAllDataForCategory = function(dataSet, category) {
-	var dataForCategory = [];
+  var dataForCategory = [];
 
-	dataSet.forEach(function(data) {
-		if (data.category === category) {
-			var vector = [];
+  dataSet.forEach(function(data) {
+    if (data.category === category) {
+      var vector = [];
 
-			for (var item in data) {
-				if (item !== 'category')
-					vector.push(Number(data[item]));
-			}
+      for (var item in data) {
+        if (item !== 'category')
+          vector.push(Number(data[item]));
+      }
 
-			dataForCategory.push(vector);
-		}
-	});
+      dataForCategory.push(vector);
+    }
+  });
 
-	return dataForCategory;
+  return dataForCategory;
 };
 
 Array.prototype.diff = function(a) {
