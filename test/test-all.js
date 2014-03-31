2d8ad2f653664f0d4a9fa73ef5aed4f17991b509
BenBlack <benjyblack@gmail.com>
2014-03-31 09:46:17 -0400

    add ten-fold cross validation, add linear bayesian

--- test/test-all.js
+++ test/test-all.js
@@ -3,7 +3,8 @@
 
 var winston = require('winston'),
 	csv = require('csv'),
-	testFunctions = require('./test-functions');
+	testFunctions = require('./test-functions'),
+	bayes = require('../lib/bayes');
 
 var files = [
 	{
@@ -21,7 +22,7 @@ var files = [
 	}
 ];
 
-var bayesTypes = ['optimal', 'naive'];
+var bayesTypes = ['optimal', 'naive', 'linear'];
 
 files.forEach(function (file) {
 	var headerline = [];
@@ -51,8 +52,50 @@ files.forEach(function (file) {
 			bayesTypes.forEach(function (bayesType) {
 				file.results[bayesType] = 0;
 				file.categories.forEach(function (primaryCategory) {
-					var result = testFunctions.runClassifier(dataSet, primaryCategory, bayesType);
-					file.results[bayesType] += result.percentCorrect;
+					// initialize classifier
+					var classifier = bayes();
+
+					var organizedData, sets;
+
+					// repeat for as long as our categories are empty
+					do {
+						dataSet = testFunctions.shuffle(dataSet);
+						sets = testFunctions.partitionSets(dataSet, 0, dataSet.length/2);
+
+						// get all categories
+						var categories = testFunctions.getCategories(dataSet);
+
+						organizedData = testFunctions.organizeData(sets.trainingSet, primaryCategory, categories);
+					} while(organizedData.primaryCategoryData.length === 0 || organizedData.secondaryCategoryData.length === 0);
+
+					// teach the classifier about the data
+					classifier.teachPrimaryCategory(organizedData.primaryCategoryData);
+					classifier.teachSecondaryCategory(organizedData.secondaryCategoryData);
+
+					if (bayesType === 'naive')
+						classifier.naivify();
+
+					var numCorrect = 0;
+					var numWrong = 0;
+
+					// Go through each data point in the testing set to see if our algorithm is correct
+					sets.testingSet.forEach(function(dataPoint) {
+						var c;
+
+						if (bayesType === 'linear') c= classifier.classifyLinear(dataPoint);
+						else c = classifier.classify(dataPoint);
+
+						if (c > 0) {
+							if (dataPoint.category === primaryCategory) numCorrect++;
+							else numWrong++;
+						}
+						else {
+							if (dataPoint.category !== primaryCategory) numCorrect++;
+							else numWrong++;
+						}
+					});
+
+					file.results[bayesType] += numCorrect/sets.testingSet.length*100;
 				});
 				winston.info('Algorithm: %s, Average percent: %d%', bayesType,file.results[bayesType]/file.categories.length);
 			});
--- test/test-functions.js
+++ test/test-functions.js
@@ -1,73 +1,32 @@
 /*jslint node: true */
 'use strict';
 
-var bayes = require('../lib/bayes'),
-	winston = require('winston');
+var organizeData = function(trainingSet, primaryCategory, categories) {
+	var primaryCategoryData, secondaryCategoryData;
 
-var runClassifier = function(dataSet, primaryCategory, bayesType) {
-	var trainingSet, testingSet, categories, primaryCategoryData, otherCategoryData;
+	// get the data for the primary category and for the secondary category
+	primaryCategoryData = getAllDataForCategory(trainingSet, primaryCategory);
 
-	do {
-		// shuffle dataSet
-		dataSet = shuffle(dataSet);
-
-		// get all categories
-		categories = getCategories(dataSet);
-
-		// check to make sure the primary category entered from command line is actually one of the categories
-		if (categories.indexOf(primaryCategory) === -1) {
-			winston.error('Given class does not exist: ' + primaryCategory);
-			return;
-		}
-
-		// initialize data sets
-		trainingSet = dataSet.slice(0,dataSet.length/2);
-		testingSet = dataSet.slice(dataSet.length/2, dataSet.length);
-
-		// get the data for the primary category and for the secondary category
-		primaryCategoryData = getAllDataForCategory(trainingSet, primaryCategory);
-		
-		otherCategoryData = [];
-		categories.forEach(function(category) {
-			if (category !== primaryCategory) {
-				otherCategoryData = otherCategoryData.concat(getAllDataForCategory(trainingSet, category));
-			}
-		});
-	} while(primaryCategoryData.length === 0 || otherCategoryData.length === 0);
-
-	// initialize classifier
-	var classifier = bayes();
-
-	// teach the classifier about the data
-	classifier.teachPrimaryCategory(primaryCategoryData);
-	classifier.teachSecondaryCategory(otherCategoryData);
-
-	if (bayesType === 'naive')
-		classifier.naivify();
-
-	var numCorrect = 0;
-	var numWrong = 0;
-	var percentCorrect = 0;
-
-	testingSet.forEach(function(dataPoint) {
-		var c = classifier.classify(dataPoint);
-
-		if (c > 0) {
-			if (dataPoint.category === primaryCategory) numCorrect++;
-			else numWrong++;
-		}
-		else {
-			if (dataPoint.category !== primaryCategory) numCorrect++;
-			else numWrong++;
+	secondaryCategoryData = [];
+	categories.forEach(function(category) {
+		if (category !== primaryCategory) {
+			secondaryCategoryData = secondaryCategoryData.concat(getAllDataForCategory(trainingSet, category));
 		}
 	});
 
-	percentCorrect = numCorrect/testingSet.length*100;
+	return {
+		primaryCategoryData: primaryCategoryData,
+		secondaryCategoryData: secondaryCategoryData
+	};
+};
+
+var partitionSets = function(dataSet, start, end) {
+	var testingSet = dataSet.slice(start, end);
+	var trainingSet = dataSet.diff(testingSet);
 
 	return {
-		numCorrect: numCorrect,
-		numWrong: numWrong,
-		percentCorrect: percentCorrect
+		trainingSet: trainingSet,
+		testingSet: testingSet
 	};
 };
 
@@ -122,7 +81,12 @@ var getAllDataForCategory = function(dataSet, category) {
 	return dataForCategory;
 };
 
-module.exports.runClassifier = runClassifier;
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return a.indexOf(i) < 0;});
+};
+
+module.exports.organizeData = organizeData;
+module.exports.partitionSets = partitionSets;
 module.exports.shuffle = shuffle;
 module.exports.getCategories = getCategories;
 module.exports.getAllDataForCategory = getAllDataForCategory;
